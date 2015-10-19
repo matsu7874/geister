5e734999eafa5828dd1c4fe7fee9b7be4a1ce8d4
matsu7874 <mtsmtkmt@gmail.com>
2015-10-19 22:55:39 +0900

    update ai_play() to calculate 1000 time match

--- ai_play.py
+++ ai_play.py
@@ -2,31 +2,35 @@ import player
 import geister
 
 
-def manual_play():
-    players = [player.AiPlayer('AI'), player.Player('Bob')]
-    g = geister.Geister(players)
-    while not g.is_finish():
-        status = g.get_status()
-        if status['turn'] % 2 == 0:
-            cells = [['-' for j in range(6)] for i in range(6)]
-            for y, x in status['active_goods']:
-                cells[y][x] = 'g'
-            for y, x in status['active_evils']:
-                cells[y][x] = 'e'
-            for y, x in status['opponent_goods']:
-                cells[y][x] = 'X'
-            for y, x in status['opponent_evils']:
-                cells[y][x] = 'X'
+def ai_play():
+    players = [player.AiPlayer('AI'), player.Player('Random')]
+    result = [{} for i in range(2)]
+    for i in range(1000):
+        g = geister.Geister(players)
+        while not g.is_finish():
+            status = g.get_status()
+            if status['turn'] % 2 == 0:
+                cells = [['-' for j in range(6)] for i in range(6)]
+                for y, x in status['active_goods']:
+                    cells[y][x] = 'g'
+                for y, x in status['active_evils']:
+                    cells[y][x] = 'e'
+                for y, x in status['opponent_goods']:
+                    cells[y][x] = 'X'
+                for y, x in status['opponent_evils']:
+                    cells[y][x] = 'X'
 
-            print('yx012345')
-            for i in range(5, -1, -1):
-                print(i, ''.join(cells[i]))
-            print('yx012345')
-            print(status['captured'])
-        g.play()
-    status = g.get_status()
-    print(status)
-    print(players[status['winner']].get_name(), status['reason'])
+            g.play()
+        status = g.get_status()
+        if status['reason'] in result[status['winner']]:
+            result[status['winner']][status['reason']] += 1
+        else:
+            result[status['winner']].update({status['reason']:1})
+        if i%100==0:
+            print(i)
+    for i in range(2):
+        for k,v in result[i].items():
+            print(i,k,v)
 
 if __name__ == '__main__':
-    manual_play()
+    ai_play()
