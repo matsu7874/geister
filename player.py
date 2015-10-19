ac71c6ab0cbce51812164080ad162c1f4dc61652
matsu7874 <mtsmtkmt@gmail.com>
2015-10-19 22:36:33 +0900

    add class AiPlayer()

--- player.py
+++ player.py
@@ -67,3 +67,51 @@ class ManualPlayer(Player):
                 sy, sx, ty, tx = map(int, s.split())
 
         return (sy, sx, ty, tx)
+
+
+class AiPlayer(Player):
+
+    def decide_initial_placement(self):
+        placement = 'eeeegggg'
+        for i in range(8):
+            r = random.randint(0, 8)
+            placement = placement[:r] + placement[r:]
+        return placement
+
+    def choice_move(self, goods, evils, enemies, captured):
+        d = ((1, 0), (-1, 0), (0, 1), (0, -1))
+        queue = []
+        my_ghosts = []
+        for g in goods:
+            my_ghosts.append(g)
+        for e in evils:
+            my_ghosts.append(e)
+        for y, x in my_ghosts:
+            for dy, dx in d:
+                if 0 <= y + dy < 6 and 0 <= x + dx < 6:
+                    if (y + dy, x + dx) not in my_ghosts:
+                        queue.append((y, x, y + dy, x + dx))
+        pointed_queue = []
+        for sy, sx, ty, tx in queue:
+            score = 0
+            if (sy, sx) in enemies:
+                score -= len(goods) + len(evils)
+            if (ty, tx) in enemies:
+                if captured['evil'] < 3:
+                    score -= 10
+                else:
+                    score += 5
+            for y, x in goods:
+                if sy == y and sx == x:
+                    score += (5 - ty)**2 + min(tx**2, (5 - tx)**2)
+                else:
+                    score += (5 - y)**2 + min(tx**2, (5 - x)**2)
+                for ey, ex in enemies:
+                    for dy, dx in d:
+                        if ey == y + dy and ex == x + dx:
+                            score += 15
+
+            pointed_queue.append((score, (sy, sx, ty, tx)))
+
+        pointed_queue.sort()
+        return pointed_queue[0][1]
