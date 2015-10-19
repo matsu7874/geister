6f203c21e8cffc803d63f88e5aca1e6657bac2d0
matsu7874 <mtsmtkmt@gmail.com>
2015-10-19 22:36:14 +0900

    add ai_play.py

---
+++ ai_play.py
@@ -0,0 +1,32 @@
+import player
+import geister
+
+
+def manual_play():
+    players = [player.AiPlayer('AI'), player.Player('Bob')]
+    g = geister.Geister(players)
+    while not g.is_finish():
+        status = g.get_status()
+        if status['turn'] % 2 == 0:
+            cells = [['-' for j in range(6)] for i in range(6)]
+            for y, x in status['active_goods']:
+                cells[y][x] = 'g'
+            for y, x in status['active_evils']:
+                cells[y][x] = 'e'
+            for y, x in status['opponent_goods']:
+                cells[y][x] = 'X'
+            for y, x in status['opponent_evils']:
+                cells[y][x] = 'X'
+
+            print('yx012345')
+            for i in range(5, -1, -1):
+                print(i, ''.join(cells[i]))
+            print('yx012345')
+            print(status['captured'])
+        g.play()
+    status = g.get_status()
+    print(status)
+    print(players[status['winner']].get_name(), status['reason'])
+
+if __name__ == '__main__':
+    manual_play()
