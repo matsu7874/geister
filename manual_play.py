0430c7c72159c105f2b1c8fd3d8090072683db18
matsu7874 <mtsmtkmt@gmail.com>
2015-10-19 21:37:01 +0900

    fix display to obscure opponent's ghosts

--- manual_play.py
+++ manual_play.py
@@ -7,21 +7,22 @@ def manual_play():
     g = geister.Geister(players)
     while not g.is_finish():
         status = g.get_status()
-        cells = [['-' for j in range(6)] for i in range(6)]
-        for y,x in status['active_goods']:
-            cells[y][x]='g'
-        for y,x in status['active_evils']:
-            cells[y][x]='e'
-        for y,x in status['opponent_goods']:
-            cells[y][x]='X'
-        for y,x in status['opponent_evils']:
-            cells[y][x]='X'
+        if status['turn'] %2 == 0:
+            cells = [['-' for j in range(6)] for i in range(6)]
+            for y,x in status['active_goods']:
+                cells[y][x]='g'
+            for y,x in status['active_evils']:
+                cells[y][x]='e'
+            for y,x in status['opponent_goods']:
+                cells[y][x]='X'
+            for y,x in status['opponent_evils']:
+                cells[y][x]='X'
 
-        print('yx012345')
-        for i in range(5,-1,-1):
-            print(i,''.join(cells[i]))
-        print('yx012345')
-        print(status['captured'])
+            print('yx012345')
+            for i in range(5,-1,-1):
+                print(i,''.join(cells[i]))
+            print('yx012345')
+            print(status['captured'])
         g.play()
     status = g.get_status()
     print(status)
