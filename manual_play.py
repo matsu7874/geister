4ea3c716050fb87db07b004897dcf2fb15c6e1d7
matsu7874 <mtsmtkmt@gmail.com>
2015-10-19 21:20:34 +0900

    update playability

--- manual_play.py
+++ manual_play.py
@@ -2,13 +2,30 @@ import player
 import geister
 
 
-def play():
+def manual_play():
     players = [player.ManualPlayer(), player.Player()]
     g = geister.Geister(players)
     while not g.is_finish():
-        print(g.get_status())
+        status = g.get_status()
+        cells = [['-' for j in range(6)] for i in range(6)]
+        for y,x in status['active_goods']:
+            cells[y][x]='g'
+        for y,x in status['active_evils']:
+            cells[y][x]='e'
+        for y,x in status['opponent_goods']:
+            cells[y][x]='X'
+        for y,x in status['opponent_evils']:
+            cells[y][x]='X'
+
+        print('yx012345')
+        for i in range(5,-1,-1):
+            print(i,''.join(cells[i]))
+        print('yx012345')
+        print(status['captured'])
         g.play()
-    print(g.get_status())
+    status = g.get_status()
+    print(status)
+    print(status['winner'],status['reason'])
 
 if __name__ == '__main__':
-    play()
+    manual_play()
