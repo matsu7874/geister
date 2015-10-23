e87a893b952accab56d524a1785fbf15103321e3
matsu7874 <mtsmtkmt@gmail.com>
2015-10-23 11:13:05 +0900

    update to create kif file

--- ai_play.py
+++ ai_play.py
@@ -1,11 +1,14 @@
 import player
 import geister
+import time
 
 
 def ai_play():
-    players = [player.AiPlayer('AI'), player.Player('Random')]
+    players = [player.AiPlayer(), player.AiPlayer()]
     result = [{} for i in range(2)]
-    for i in range(1000):
+    file_name = 'kif_'+time.strftime("%Y%m%d_%H%M%S", time.gmtime())+'.json'
+    for i in range(1,1001):
+    # for i in range(1,101):
         g = geister.Geister(players)
         while not g.is_finish():
             status = g.get_status()
@@ -34,6 +37,12 @@ def ai_play():
                     print(i, k, v)
                     win[i] += v
             print(win[0], '/', win[1])
+        if status['reason'] == 'over 500 turn':
+            pass
+        else:
+            f = open(file_name,'a')
+            f.write(g.get_score()+'\n')
+            f.close()
     win = [0, 0]
     for i in range(2):
         for k, v in result[i].items():
